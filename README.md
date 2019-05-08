dd6f47b61c75af74a6d2c462730f7c4dcd590f88
Ville-Veikko Saari <ville-veikko.saari@helsinki.fi>
2019-05-08 14:34:59 +0300

    argparser and readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-Dramaturg.io
+#Dramaturg.io
+
+
+#### Running the project
+```bash
+$ python main.py -h
+usage: main.py [-h] [-s] [someargument]
+
+positional arguments:
+  someargument
+
+optional arguments:
+  -h, --help    show this help message and exit
+  -s, --story   Create a story! (default: False)
+```
+
+#### Running a module on its own:
+```bash
+$ python -m story.story
+
+$ python -m scene.scene
+
+$ python -m sequence.sequence
+
+$ python -m adjacency_pair.adjacency_pair
+
+$ python -m language.language
+```
--- main.py
+++ main.py
@@ -1,4 +1,25 @@
+import argparse
+
 from story.story import Story
 
-story = Story()
-print(story)
+
+def main(do_story):
+    if do_story:
+        story = Story()
+        print(story)
+    else:
+        print("Did nothing!")
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("someargument", nargs="?", default=None)
+    parser.add_argument("-s", "--story", help="Create a story!", action='store_true')
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    print(args)
+    main(args.story)
