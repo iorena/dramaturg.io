a486642a632e918d97643c6a5e7f31047ff62b47
Salmenkivi Essi E <essi.salmenkivi@.helsinki.fi>
2019-06-18 12:57:14 +0300

    fix handling of empty csv cells

--- sequence/sequence_types.py
+++ sequence/sequence_types.py
@@ -8,6 +8,6 @@ class SequenceType:
         with open("sequence/sequence_types.csv") as csv_file:
             csv_reader = csv.reader(csv_file, delimiter="\t")
             for row in csv_reader:
-                pos_sequences[row[0]] = (row[1], row[2])
-                neg_sequences[row[0]] = (row[3], row[4])
+                pos_sequences[row[0]] = (row[1], None if row[2] == "" else row[2])
+                neg_sequences[row[0]] = (row[3], None if row[4] == "" else row[4])
         return pos_sequences, neg_sequences
