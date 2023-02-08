d051859ac3848958ffda40136fa1285e43540b72
Juri Kuronen <juri.kuronen@gmail.com>
2023-02-08 18:12:59 +0200

    Add average function.

--- scripts/lib/Score.pm
+++ scripts/lib/Score.pm
@@ -36,23 +36,9 @@ sub add_word_counts($document, $score_keeper) {
 }
 
 # Compound words are treated as the average of their parts.
-sub score_word($score_keeper, $word) {
-    my @parts = Word::get_word_parts($word);
-    my $score = 0;
-    $score += $score_keeper->{'word_counts'}->{$_} for @parts;
-    return $score / @parts;
-}
+sub score_word($score_keeper, $word) { return Utils::average(map { $score_keeper->{'word_counts'}->{$_} } word::get_word_parts($word)); }
 
 # Score a list of words such that only nouns and proper nouns count for the score.
-sub score_words($score_keeper, @words) {
-    my ($n, $score) = (0) x 2;
-    for my $word (@words) {
-        if (Word::is_noun($word) || Word::is_propn($word)) {
-            $score += score_word($score_keeper, $word);
-            ++$n;
-        }
-    }
-    return $n > 0 ? $score / $n : 0;
-}
+sub score_words($score_keeper, @words) { return Utils::average(map { score_word($score_keeper, $_) } grep { Word::is_noun($_) || Word::is_propn($_) } @words); }
 
 1;
--- scripts/lib/Utils.pm
+++ scripts/lib/Utils.pm
@@ -18,6 +18,12 @@ sub intsort(@values) { return sort { $a <=> $b } @values; }
 
 sub precision($x, $p) { return int($x * (10 ** $p) + 0.5) / (10 ** $p); }
 
+sub average(@vals) {
+    my ($avg, $n) = (0) x 2;
+    $avg += $_ for @vals;
+    return $n > 0 ? $avg / $n : 0;
+}
+
 sub word_ids_to_text($graph, @ids) { return ("\"" . join(' ', map { Word::form(Graph::get_word($graph, $_)) } intsort @ids) . "\"") =~ s/\s+([,.;:!?])/$1/gr; }
 
 sub remove_hashtag($str) { return $str =~ s/#//gr; }
