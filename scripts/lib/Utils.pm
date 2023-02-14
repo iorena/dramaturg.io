4c6bf7ce31a73cbea3aeeb1dcc6541ec71087ecc
Juri Kuronen <juri.kuronen@gmail.com>
2023-02-14 13:51:36 +0200

    Handle lower case.

--- scripts/lib/Utils.pm
+++ scripts/lib/Utils.pm
@@ -6,11 +6,15 @@ use utf8;
 no warnings "experimental::signatures";
 use feature qw(postderef signatures);
 
+use locale;
+
 use File::Basename;
 use lib dirname (__FILE__);
 
 use Graph;
 use Word;
+
+use Encode qw(encode decode);
 
 package Utils;
 
@@ -25,8 +29,10 @@ sub average(@vals) {
     return $n > 0 ? $avg / $n : 0;
 }
 
-sub word_ids_to_text($graph, @ids) { return ("\"" . join(' ', map { Word::form(Graph::get_word($graph, $_)) } intsort @ids) . "\"") =~ s/\s+([,.;:!?])/$1/gr; }
+sub word_ids_to_text($graph, @ids) { return ("\"" . join(" ", map { Word::form(Graph::get_word($graph, $_)) } intsort @ids) . "\"") =~ s/\s+([,.;:!?])/$1/gr; }
 
 sub remove_hashtag($str) { return $str =~ s/#//gr; }
+
+sub lower_case($str) { return Encode::encode("utf-8", lc Encode::decode("utf-8", $str)); }
 
 1;
