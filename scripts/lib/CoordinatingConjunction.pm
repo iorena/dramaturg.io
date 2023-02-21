2085e2be89bc93a935ab7c725eb73d8fe7b363b0
Juri Kuronen <juri.kuronen@gmail.com>
2023-02-21 13:10:35 +0200

    Add forgotten package.

--- scripts/lib/CoordinatingConjunction.pm
+++ scripts/lib/CoordinatingConjunction.pm
@@ -16,8 +16,7 @@ package CoordinatingConjunction;
 
 sub get_cc_parts($graph, $word) {
     my @cc_parts;
-    my $id = Word::id($word);
-    for my $conj_id (Graph::get_radj_ids_if($graph, $id, \&Word::is_conj)) {
+    for my $conj_id (Graph::get_radj_ids_if($graph, Word::id($word), \&Word::is_conj)) {
         my @ccs = Graph::get_radj_ids_if($graph, $conj_id, \&Word::is_cc);
         push @cc_parts, ($conj_id, @ccs) if @ccs;
     }
