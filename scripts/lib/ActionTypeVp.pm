ae43a88f4236b0c69e2fbe392e5c3721a66f0d76
Juri Kuronen <juri.kuronen@gmail.com>
2023-03-05 21:56:49 +0200

    Use clause graph to parse vps.

--- scripts/lib/ActionTypeVp.pm
+++ scripts/lib/ActionTypeVp.pm
@@ -11,95 +11,30 @@ use lib dirname (__FILE__);
 
 use Conjunction;
 use Graph;
+use SubordinateClause;
 use Utils;
 use Word;
 
 package ActionTypeVp;
 
-sub collect_subordinate_clause($sclause, $graph, $i, @stop) {
-    for my $id (map { Word::id($_) } grep { !Conjunction::is_conjunction_word($graph, $_) } Graph::get_radj($graph, $i)) {
-        if (!grep { /^$id$/ } @stop) {
-            push @{$sclause}, $id;
-            collect_subordinate_clause($sclause, $graph, $id, @stop);
-        }
-    }
-}
-
-sub get_subordinate_clauses($graph, @sconj_heads) {
-    my @sclauses;
-    for (@sconj_heads) {
-        my @sclause = ($_);
-        collect_subordinate_clause(\@sclause, $graph, $_, @sconj_heads);
-        push @sclauses, \@sclause;
-    }
-    return @sclauses;
-}
-
-# Skip over "cop" and "case" deprels. Also "mark.
-sub not_clause_start($word) { return Word::is_cop($word) || Word::is_case($word) || Word::is_mark($word); }
-
-# Get "SCONJ" "HEAD" to point to clause start.
-sub get_sconj_head($graph, $sconj_id) {
-    do {
-        $sconj_id = Word::head(Graph::get_word($graph, $sconj_id));
-    } while ($sconj_id > 0 && not_clause_start(Graph::get_word($graph, $sconj_id)));
-    return $sconj_id;
-}
-
-# Can be checked via "SCONJ" UPOS or "mark" DEPREL. The former is usually also the latter.
-sub get_sconj($word) { return Word::is_sconj($word) || Word::is_mark($word); }
-
 sub add_sclause_vp($action_type, $graph, $sclause, $verb_id) {
     my $vp = $sclause->[0] < $verb_id ? 'pre_vp' : 'post_vp';
-    ActionType::add_value($action_type, $vp, join(" ", map { Word::form($_) } grep { !Word::is_punct($_) } map { Graph::get_word($graph, $_) } Utils::intsort $_->@*));
+    ActionType::add_value($action_type, $vp, join(" ", map { Word::form($_) } grep { !Word::is_punct($_) } map { Graph::get_word($graph, $_) } Utils::intsort $sclause->@*));
 }
 
 sub process_vp($action_type, $graph, $clause_graph, $verb_id) {
     # Check if clause can be further split via subordinating conjunctions to subordinate clauses, and collect them.
-    my @sclauses = get_subordinate_clauses($graph, map { get_sconj_head($graph, $_) } grep { get_sconj(Graph::get_word($graph, $_)) } $clause_graph->{$verb_id}->{'word_ids'}->@*);
-    add_sclause_vp($action_type, $graph, $_, $verb_id) for @sclauses;
+    my @sclauses = SubordinateClause::get_subordinate_clauses($graph, $clause_graph, $verb_id);
+    my $subject_id = $action_type->{'subject_id'};
+    my $object_id = $action_type->{'object_id'};
+    for (@sclauses) {
+        next if (grep { $_ eq $subject_id || $_ eq $object_id || $_ eq $verb_id } $_->@*);
+        add_sclause_vp($action_type, $graph, $_, $verb_id) 
+    }
     
+    # More vp types to be processed soon.
+
     return;
-
-# skip midvp constructions
-
-    # Check simpler "one-word" vps.
-    my @bad_ids = map { $_->@* } @sclauses;
-    my (@pre_stuff, @post_stuff);
-    for my $id ($clause_graph->{$verb_id}->@*) {
-        next if grep { $id == $_ } @bad_ids;
-        my $word = Graph::get_word($graph, $id);
-        # ADV's handled simpler
-        if (Word::is_adv($word) || Word::is_adp($word)) {
-            my $arr = ($id < $verb_id ? \@pre_stuff : \@post_stuff);
-            push $arr->@*, $id;
-            my $head = Word::head($word);
-            my $head_word = Graph::get_word($graph, $head);
-            #
-            if (Word::is_obl($head_word)) {
-                push $arr->@*, $head;
-                my @conjs = Graph::get_radj_ids_if($graph, $head, \&Word::is_conj);
-                my @cconjs = map { Graph::get_radj_if($graph, $_, \&Word::is_cconj) } @conjs;
-                push $arr->@*, Word::head($_) for @cconjs;
-            }
-            #
-            if (Word::is_acl($head_word)) {
-                push $arr->@*, $head;
-                my @nsubjs = Graph::get_radj_ids_if($graph, $head, \&Word::is_nsubj);
-                my @obls = Graph::get_radj_ids_if($graph, $head, \&Word::is_obl);
-                push $arr->@*, $_ for @nsubjs;
-                push $arr->@*, $_ for @obls;
-            }
-        }
-    }
-    if (@pre_stuff) {
-        @pre_stuff = Utils::intsort @pre_stuff;
-        ActionType::add_value($action_type, 'pre_vp', join(" ", map { Word::form(Graph::get_word($graph, $_)); } $pre_stuff[0]..$pre_stuff[-1]));
-    }
-    if (@post_stuff) {
-        @post_stuff = Utils::intsort @post_stuff;
-        ActionType::add_value($action_type, 'post_vp', join(" ", map { Word::form(Graph::get_word($graph, $_)); } $post_stuff[0]..$post_stuff[-1]));
-    }
 
 }
 
--- scripts/lib/ClauseGraph.pm
+++ scripts/lib/ClauseGraph.pm
@@ -13,6 +13,10 @@ use Conjunction;
 use Utils;
 use Word;
 
+# ClauseGraph assembles the sentence text into (clause) nodes that contain exclusive lists of words
+# and then connects the nodes together. However, the interpretation of "clause" is extended as needed
+# to suit the functionality of this program and as such might not match any real-world definitions of what a clause is.
+
 package ClauseGraph;
 
 sub clause_node($word_id, $parent_id) {
@@ -24,8 +28,8 @@ sub clause_node($word_id, $parent_id) {
     };
 }
 
-# Create and connect clause nodes with a recursive DFS through the dependency graph.
-sub connect_conjunctions($graph, $clause_graph, $clause_node_id, $id) {
+# Recursive DFS through the dependency graph which constructs and connects new clause nodes at conjunction words.
+sub construct_clause_graph($graph, $clause_graph, $clause_node_id, $id) {
     for my $next_id (Graph::get_radj_ids($graph, $id)) {
         if (Conjunction::is_conjunction_word($graph, Graph::get_word($graph, $next_id))) {
             # Word is conjunction: create new clause node.
@@ -35,59 +39,53 @@ sub connect_conjunctions($graph, $clause_graph, $clause_node_id, $id) {
             push $clause_graph->{$clause_node_id}->{'adj'}->@*, $next_id;
 
             # Continue search with new clause node.
-            connect_conjunctions($graph, $clause_graph, $next_id, $next_id);
+            construct_clause_graph($graph, $clause_graph, $next_id, $next_id);
         } else {
             # Add word to clause node's word list.
             push $clause_graph->{$clause_node_id}->{'word_ids'}->@*, $next_id;
 
             # Continue searching with this clause node.
-            connect_conjunctions($graph, $clause_graph, $clause_node_id, $next_id);
+            construct_clause_graph($graph, $clause_graph, $clause_node_id, $next_id);
         }
     }
 }
 
-sub combine_adj($graph, $clause_graph, $id) {
-    my @new_adj;
-    my @adj = get_adj($clause_graph, $id);
+# Merge a clause node with its children.
+sub merge_adj($graph, $clause_graph, $id) {
+    my @adj;
 
-    for my $adj_id (@adj) {
+    for my $adj_id (get_adj($clause_graph, $id)) {
+        # Get child's children.
         my @adj2 = get_adj($clause_graph, $adj_id);
-        for (@adj2) {
-            push @new_adj, $_;
-            $clause_graph->{$_}->{'parent_id'} = $id;
-        }
+
+        # They will be the clause node's new children.
+        push @adj, @adj2;
+        $clause_graph->{$_}->{'parent_id'} = $id for @adj2;
+
+        # Copy word_ids.
         push $clause_graph->{$id}->{'word_ids'}->@*, $clause_graph->{$adj_id}->{'word_ids'}->@*;
+
+        # Delete child.
         delete $clause_graph->{$adj_id};
     }
-    $clause_graph->{$id}->{'adj'}->@* = @new_adj;
+
+    $clause_graph->{$id}->{'adj'}->@* = @adj;
 }
 
-# Search through clause nodes (i.e. conjunction words) in the clause graph recursively and combine nodes which correspond to list structures.
+# Merge clause nodes at adjacent levels if they correspond to list structures in the sentence.
 sub detect_list_structures($graph, $clause_graph, $id) {
     my @adj = get_adj($clause_graph, $id);
     if (@adj == 0) {
         # Stop search if arrived at leaf node.
         return;
-    } elsif (@adj == 1) {
-        if (Word::is_verb(Graph::get_word($graph, $adj[0]))) {
-            # Continue search if links to a verbal conjunction.
-            detect_list_structures($graph, $clause_graph, $adj[0]);
-        } else {
-            # Combine non-verbal conjunction to this node.
-            combine_adj($graph, $clause_graph, $id);
-
-            # Search this node again after combining adj.
-            detect_list_structures($graph, $clause_graph, $id);
-        }
-    } else {
-        my @verbs = grep { Word::is_verb(Graph::get_word($graph, $_)) } @adj;
-
-        if (@verbs) {
-            # ?
+    } elsif (@adj > 0) {
+        # Merge if the clause node is only adjacent to non-verbal conjunction words.
+        if (grep { Word::is_verb(Graph::get_word($graph, $_)) } @adj) {
+            # Contains any verbal conjunction words: continue search normally from all children.
             detect_list_structures($graph, $clause_graph, $_) for @adj;
         } else {
-            # Combine non-verbal conjunctions which indicate a list structure.
-            combine_adj($graph, $clause_graph, $id);
+            # Merge non-verbal conjunctions which indicate a list structure.
+            merge_adj($graph, $clause_graph, $id);
 
             # Search this node again after combining adj.
             detect_list_structures($graph, $clause_graph, $id);
@@ -102,8 +100,10 @@ sub clause_graph($graph) {
         $root_id => clause_node($root_id, undef)
     );
 
-    connect_conjunctions($graph, \%clause_graph, $root_id, $root_id);
+    # Recursive DFS through the dependency graph which constructs and connects new clause nodes at conjunction words.
+    construct_clause_graph($graph, \%clause_graph, $root_id, $root_id);
 
+    # Merge clause nodes at adjacent levels if they correspond to list structures in the sentence.
     detect_list_structures($graph, \%clause_graph, $root_id);
 
     return %clause_graph;
